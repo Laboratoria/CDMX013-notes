e85249b9fde81a57f93a54d6bbdc1f641a91d53e
Miriam Cortes <miriamcortes.06@gmail.com>
2022-12-02 17:52:23 -0600

    Last Changes

--- notes-taking/src/App.js
+++ notes-taking/src/App.js
@@ -1,31 +1,30 @@
 import {useState} from "react";
 import {Route, Routes} from "react-router-dom"
-import React,{useEffect} from 'react';
-import {collection, getDocs} from "firebase/firestore";
-import db from "./firebase/config";
-import Login from "./componentes/no-auth/Login";
-import Wall from "./componentes/wall/Wall";
-//import { async } from "@firebase/util";
 
+import { collection, getDocs } from 'firebase/firestore';
+import db from './firebase/config';
+import Login from './componentes/no-auth/Login';
+import Wall from './componentes/wall/Wall';
+// import { async } from "@firebase/util";
 
-function App (){
-  useEffect(()=>{
-    const getData=async()=>{
-      const data =await getDocs(collection(db, 'usuarios'));
-      console.log(data);
+function App () {
+  useEffect(() => {
+    const getData = async () => {
+      const data = await getDocs(collection(db, 'usuarios'))
+      console.log(data)
     }
-    getData();
+    getData()
     
-    }, []);
-    const [user, setUser] = useState(undefined);
+    }, [])
+    const [user, setUser] = useState(undefined)
     return (
 <Routes>
-  {user?
-  <Route path="/" element={<Wall/>}/>: 
-  <Route path="/" element={<Login setUser={setUser}/>}/> 
-  } 
+  {user ?
+  <Route path="/" element={<Wall/>}/> :
+  <Route path="/" element={<Login setUser={setUser}/>}/>
+  }
 </Routes>
-   
-  );
+
+  )
 };
-export default App;
+export default App
