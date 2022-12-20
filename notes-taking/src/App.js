5bddb9581744de7771b51587282a80808deef9db
Miriam Cortes <miriamcortes.06@gmail.com>
2022-12-19 21:13:19 -0600

    editar

--- notes-taking/src/App.js
+++ notes-taking/src/App.js
@@ -4,17 +4,22 @@ import { Route, Routes } from "react-router-dom"
 
 import Login from './componentes/no-auth/Login';
 import Wall from './componentes/wall/Wall';
+import EditNotes from './componentes/EditNotes/EditNotes'
 
 // import db from './firebase/config';
 import AllNotes from './componentes/AllNotes/AllNotes'
 
 function App() {
 
-  const [user, setUser] = useState(undefined)
+  const [user, setUser] = useState(null)
+
+  const [lista, setLista] = useState([]);
+
+
 
   const auth = getAuth();
 
-  onAuthStateChanged(auth, (userAuth) => {
+  onAuthStateChanged (auth, (userAuth) => {
     if (userAuth) {
       setUser(userAuth)
     } else {
@@ -31,10 +36,10 @@ function App() {
       <Route path="/" element={<Login setUser={setUser} />} />
       <Route path="/welcome" element={user ? <AllNotes exit={userNull} /> : <Login setUser={setUser} />} />
       <Route path="/wall" element={user?<Wall exit={userNull}/>:<Login setUser={setUser}/>}/>
-      <Route path="/allNotes" element={user?<AllNotes exit={userNull}/>:<Login setUser={setUser}/>}/>
+      <Route path="/allNotes" element={user?<AllNotes exit={userNull} lista={lista} setLista={setLista}/>:<Login setUser={setUser}/>}/>
+      <Route path="/editNotes/:id" element={user?<EditNotes lista={lista}/>:<Login setUser={setUser}/>}/>
     </Routes>
 
   )
 };
-
 export default App;
--- notes-taking/src/componentes/AllNotes/AllNotes.jsx
+++ notes-taking/src/componentes/AllNotes/AllNotes.jsx
@@ -1,6 +1,6 @@
-import { useNavigate } from "react-router-dom";
-import { useState, useEffect } from "react";
-import "../no-auth/login.css";
+import { useNavigate } from 'react-router-dom'
+import { useState, useEffect } from 'react'
+import '../no-auth/login.css'
 import {
   getFirestore,
   collection,
@@ -9,110 +9,117 @@ import {
   deleteDoc,
   doc,
   getDoc,
-  setDoc,
-} from "firebase/firestore";
-import { signOutAccount } from "../google";
-import Welcome from "../Welcome/welcome";
-import { getAuth } from "firebase/auth";
-import { app } from "../../firebase/config";
+  setDoc
+} from 'firebase/firestore'
+import { signOutAccount } from '../google'
+import Welcome from '../Welcome/welcome'
+import { getAuth } from 'firebase/auth'
+import { app } from '../../firebase/config'
+import { editableInputTypes } from '@testing-library/user-event/dist/utils'
 
-const db = getFirestore(app);
-const auth = getAuth(app);
+const db = getFirestore(app)
+const auth = getAuth(app)
 
-export default function AllNotes(props, { userEmail }) {
-  const navigate = useNavigate();
-  const exit = props.exit;
-  const images = require.context("../../img", true);
+export default function AllNotes (props) {
+  const navigate = useNavigate()
+  const exit = props.exit
+  const images = require.context('../../img', true)
 
   const signOutA = async () => {
-    await signOutAccount();
-    exit();
+    await signOutAccount()
+    exit()
 
-    navigate("/");
-    console.log("ya me fui");
-  };
+    navigate('/')
+    console.log('ya me fui')
+  }
 
   const back = () => {
-    navigate("/wall");
-  };
+    navigate('/wall')
+  }
 
-  const [lista, setLista] = useState(null);
+  const [lista, setLista] = useState(null)
 
   useEffect(() => {
     const getLista = async () => {
       try {
-        const querySnapshot = await getDocs(collection(db, "usuarios"));
-        const docs = [];
-        querySnapshot.forEach((doc) => {
-          docs.push({ ...doc.data(), id: doc.id });
-        });
-        setLista(docs);
+        const querySnapshot = await getDocs(collection(db, 'usuarios'))
+        const docs = []
+        querySnapshot.forEach(doc => {
+          docs.push({ ...doc.data(), id: doc.id })
+        })
+        setLista(docs)
       } catch (error) {
-        console.log(error);
+        console.log(error)
       }
-    };
-    getLista();
-  }, []);
+    }
+    getLista()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
 
   //función para delete
   // if (auth.currentUser.id === doc.id) {
-  const deletePost = async (id) => {
-    await deleteDoc(doc(db, "usuarios", id));
-  };
-
-  // deletePost.addEventListener('click', async () => {
-  //   await deletePost(doc.id);
-  // });
+  const deleteNote = async id => {
+    await deleteDoc(doc(db, 'usuarios', id))
+    const newNoteList = [...lista.filter(item => item.id !== id)]
+    setLista(newNoteList)
+  }
+  const editBtn = async id => {
+    navigate('/editNotes/' + id)
+  }
 
   if (!lista) {
-    return <h3>Cargando...</h3>;
+    return <h3>Cargando...</h3>
   }
 
   return (
-    <div className="allNotes">
-      {/* <p>
-        <strong>{userEmail}</strong>
-      </p> */}
+    <div className='allNotes'>
+
       <h2>ALL YOUR NOTES!</h2>
-   
+
       <img
-        src={images("./logout.png")}
-        alt={""}
-        className="btn-goOut"
+        src={images('./logout.png')}
+        alt={''}
+        className='btn-goOut'
         onClick={() => {
-          signOutA();
+          signOutA()
         }}
       ></img>
 
       <img
-        src={images("./back.png")}
-        alt={""}
-        className="back"
+        src={images('./back.png')}
+        alt={''}
+        className='back'
         onClick={() => {
-          back();
+          back()
         }}
       ></img>
 
-      <div className="card-body">
+      <div className='card-body'>
         {lista.length === 0 && <Welcome />}
-        {lista.map((list) => (
-          <article className="card" key={list.id}>
+        {lista.map(list => (
+          <div className='card' key={list.id}>
             {list.title}
             {list.body}
             <button
-              className="delete"
+              className='delete'
               onClick={() => {
-                deletePost(list.id);
+                deleteNote(list.id)
               }}
             >
               Delete
             </button>
 
-            <button className="edit">Edit</button>
-          </article>
+            <button
+              className='edit'
+              onClick={() => {
+                editBtn(list.id)
+              }}
+            >
+              Edit
+            </button>
+          </div>
         ))}
-
       </div>
     </div>
-  );
+  )
 }
